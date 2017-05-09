8c259bd3a36f91ff67285156240215f043703c8f
hutgrill <ca19605@students.mcpasd.k12.wi.us>
2017-05-09 11:54:57 -0500

    Add files via upload

---
+++ ConnectFour.java
@@ -0,0 +1,42 @@
+public class Board
+{
+private final int column = 7;
+private final int row = 7;
+private int[][] board = new int[row][column];
+
+ public static void Board() {
+ 
+   for (int i=0; i<7; i++)
+   {
+      for(int j=0; j<7; j++)
+      {
+      System.out.print("|_");
+      
+      }
+   System.out.println();
+   
+   
+   }
+ 
+ 
+ 
+ 
+ 
+ 
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
