928d750b2a4545676cb019ec777501356b1e91e2
mukrohin <rohinmukherjee@hotmail.com>
2017-05-17 09:31:27 -0500

    game launcher

--- ConnectFour.java
+++ ConnectFour.java
@@ -7,13 +7,35 @@
 
 package connectFour;
 
+import java.util.Scanner;
+
 public class ConnectFour {
 
 	public static void main(String[] args) {
 		
-		Grid gird = new Grid();
-		gird.place(2, 3);
-		gird.place(1, 3);
-		System.out.println(gird.toString());
+		Grid stand = new Grid();
+		Scanner input = new Scanner(System.in);
+		int pNum = 2;
+		int c = 0;
+		
+		System.out.println(stand.toString() +"\n");
+		
+		do {
+			if(pNum == 1) {
+				pNum=2;
+			}
+			else {
+				pNum=1;
+			}
+			
+			System.out.print("Player " +pNum +" enter column number:");
+			c = Integer.parseInt(input.nextLine());
+			stand.place(pNum, c);
+			System.out.println(stand.toString() +"\n");
+		}
+		while(stand.checkForWinner(pNum) == false);
+		
+		System.out.println("Player " +pNum +" wins!");
+		input.close();
 	}
 }
